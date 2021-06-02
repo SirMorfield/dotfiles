02c814f72955c496c1e49e06148576a473dc97d4
SirMorfield <jkoers@student.codam.nl>
2021-06-03 00:24:45 +0200

    add rsync thing

---
+++ rs/rs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ "$#" -ne 2 ]; then
+	echo "Supply 2 arguments: push|pull dir"
+	exit 1
+fi
+
+if [ $(curl -s ifconfig.me) == "80.61.192.39" ]; then
+	sshcmd="'ssh'"
+	server="joppe@192.168.2.1"
+else	
+	sshcmd="'ssh -p 10001'"
+	server="joppe@joppekoers.nl"
+fi
+
+if [ $(hostname) == "laptop" ]; then
+	host="laptop"
+else
+	host="codam"
+fi
+
+if [ "$2" == "all" ]; then
+	dir="$HOME/Downloads $HOME/Desktop $HOME/GitHub"
+else
+	dir=$2
+fi
+
+if [ "$1" = "push" ]; then
+	sh -c "rsync -ahP --delete-after --delete-excluded --backup-dir ../$(echo $host)_deleted/ --links -e $sshcmd $dir $server:/home/joppe/$host/"
+else
+	echo "Invalid argument, use push|pull dir|all"
+fi
