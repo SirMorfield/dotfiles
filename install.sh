f9b76a7688ec0f74b9298077c4d74a7db5e056a4
SirMorfield <jkoers@student.codam.nl>
2021-12-21 17:15:05 +0100

    complete terminal install script

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
-brew install bat
-brew install git
+#!/bin/bash
+# brew install bat
+# brew install git
 
-git config --global user.name "SirMorfield"
-git config --global user.email "joppe.koers@gmail.com"
+# git config --global user.name "SirMorfield"
+# git config --global user.email "joppe.koers@gmail.com"
 
+sudo apt install -y zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 ln -s ~/.dotfiles/.zshrc ~
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ln -s ~/.dotfiles/.tmux.conf ~
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 yes | ~/.fzf/install
+
+sudo apt install -y bat
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
