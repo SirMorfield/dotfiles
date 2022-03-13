328904427730659626da992b7a404d65e5e1bbe5
SirMorfield <jkoers@student.codam.nl>
2022-03-13 13:36:21 +0100

    remove unsued file

--- scripts/spotify_backup/app.js
+++
@@ -1,9 +0,0 @@
-var fetch = require('node-fetch');
-
-fetch('https://api.spotify.com/v1/me/tracks', {
-	headers: {
-		'Accept': 'application/json',
-		'Content-Type': 'application/json',
-		'Authorization': 'Bearer '
-	}
-});
