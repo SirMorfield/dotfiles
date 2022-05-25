26e308f4092e0e6edca0f8dde6973f8b45dc29a6
SirMorfield <jkoers@student.codam.nl>
2022-05-26 00:30:22 +0200

    feat: cleanup unused files

--- scripts/video_merge/src/parralel.ts
+++
@@ -1,9 +0,0 @@
-// export async function mapAsyncN(arr: any[], n: number, fn: (element: any) => any) {
-// 	let offset = 0
-// 	while (true) {
-// 		for (let i = offset; i < n && i < arr.length; i++) {
-// 			arr[i] = fn(arr[i])
-// 		}
-// 		i+= n
-// 	}
-// }
